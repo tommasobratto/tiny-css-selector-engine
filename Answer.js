94982597085e8182234e43c2c48aff5a172eaae8
tommasobratto <tbratto22@gmail.com>
2015-06-02 15:00:46 +0100

    added tagFinder and idFinder functions, 3 tests passing

--- Answer.js
+++ Answer.js
@@ -1,9 +1,37 @@
 var $ = function (selector) {
   var elements = [];
 
-  ////////////////////
-  // Your code here //
-  ////////////////////
+  if (selector.includes("#")) {
+    idFinder(selector, elements);
+  } else {
+    tagFinder(selector, elements);
+  }
 
+  // console.log(elements);
   return elements;
+}
+
+function idFinder(selector, elements) {
+  var i, parts, element;
+
+  parts = selector.split(/#/);
+
+  for (i = 0; i < parts.length; i++) {
+    element = document.getElementById(parts[i]);
+
+    if (element) {
+      elements.push(element);
+    }
+  }
+}
+
+function tagFinder(selector, elements) {
+  // answer to A
+  var i, elementsFound;
+
+  elementsFound = document.getElementsByTagName(selector);
+
+  for (i = 0; i < elementsFound.length; i++) {
+    elements.push(elementsFound[i]);
+  }
 }
