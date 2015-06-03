ed1a40529afa5594e3b8d561413187f4f008a825
tommasobratto <tbratto22@gmail.com>
2015-06-03 23:55:56 +0100

    refactoring

--- Answer.js
+++ Answer.js
@@ -18,28 +18,19 @@ var $ = function (selector) {
   return elements;
 }
 
-function tagFinder(selector, elements) {
-  var i, elementsFound;
 
-  elementsFound = document.getElementsByTagName(selector);
-
-  for (i = 0; i < elementsFound.length; i++) {
-    elements.push(elementsFound[i]);
-  }
-}
-
+// 1st function tier
 function idFinder(parts, elements) {
-  var element, elementId;
+  var element;
+
   if (parts[0][0] !== "#") {
-    elementId = parts[1].replace("#", "");
-    element = document.getElementById(elementId);
+    element = getElementById(parts[1]);
 
     if (element) {
-      elementHasId(element, parts);
+      elementHasId(element, parts[1]);
     }
   } else {
-    elementId = parts[0].replace("#", "");
-    element = document.getElementById(elementId);
+    element = getElementById(parts[0]);
 
     if (element) {
       elements.push(element);
@@ -48,32 +39,16 @@ function idFinder(parts, elements) {
 }
 
 function classFinder(parts, elements) {
-  var elementsFound, elementClass;
+  var elementsFound;
 
   if (parts[0][0] !== ".") {
-    elementClass = parts[1].replace(".", "");
-    elementsFound = document.getElementsByClassName(elementClass);
+    elementsFound = getElementsByClass(parts[1]);
 
     elementHasClass(elements, elementsFound, parts);
   } else {
-    elementClass = parts[0].replace(".", "");
-    elementsFound = document.getElementsByClassName(elementClass);
+    elementsFound = getElementsByClass(parts[0])
 
     for (i = 0; i < elementsFound.length; i++) {
-      elements.push(elementsFound[i]);
-    }
-  }
-}
-
-function elementHasId(element, parts) {
-  if (element.tagName === parts[0]) {
-    elements.push(element);
-  }
-}
-
-function elementHasClass(elements, elementsFound, parts) {
-  for (i = 0; i < elementsFound.length; i++) {
-    if (elementsFound[i].tagName.toLowerCase() === parts[0]) {
       elements.push(elementsFound[i]);
     }
   }
@@ -84,35 +59,75 @@ function elementFinder(parts, elements) {
 
   if (parts[0][0] !== "#" || parts[0][0] !== ".") {
     if (parts[1][0] === "#") {
-      element = document.getElementById(parts[1].replace("#", ""));
-      classElements = document.getElementsByClassName(parts[2].replace(".", ""));
+      element = getElementById(parts[1]);
+      classElements = getElementsByClass(parts[2]);
 
-      if (element && classElements && element.tagName.toLowerCase() === parts[0]) {
+      if (element.tagName === parts[0].toUpperCase()) {
         checkIfElementSpecified(element, classElements, elements);
       }
     } else {
-      element = document.getElementById(parts[2].replace("#", ""));
-      classElements = document.getElementsByClassName(parts[1].replace(".", ""));
+      element = getElementById(parts[2]);
+      classElements = getElementsByClass(parts[1]);
 
-      if (element && classElements && element.tagName.toLowerCase() === parts[0]) {
+      if (element.tagName === parts[0].toUpperCase()) {
         checkIfElementSpecified(element, classElements, elements);
       }
     }
   } else {
     if (parts[0][0] === "#") {
-      element = document.getElementById(parts[1].replace("#", ""));
-      classElements = document.getElementsByClassName(parts[2].replace(".", ""));
+      element = getElementById(parts[1]);
+      classElements = getElementsByClass(parts[2]);
 
       if (element && classElements) {
         checkIfElementSpecified(element, classElements, elements);
       }
     } else {
-      element = document.getElementById(parts[2].replace("#", ""));
-      classElements = document.getElementsByClassName(parts[1].replace(".", ""));
+      element = getElementById(parts[1]);
+      classElements = getElementsByClass(parts[2]);
 
-      if (element && classElements) {
-        checkIfElementSpecified(element, classElements, elements);
-      }
+      checkIfElementSpecified(element, classElements, elements);
+    }
+  }
+}
+
+function tagFinder(selector, elements) {
+  var i, elementsFound;
+
+  elementsFound = document.getElementsByTagName(selector);
+
+  for (i = 0; i < elementsFound.length; i++) {
+    elements.push(elementsFound[i]);
+  }
+}
+
+
+// 2nd function tier
+function getElementById(value) {
+  var elementId;
+
+  elementId = value.replace("#", "");
+  return document.getElementById(elementId);
+}
+
+function getElementsByClass(value) {
+  var elementClass;
+
+  elementClass = value.replace(".", "");
+  return document.getElementsByClassName(elementClass);
+}
+
+
+// 3rd function tier
+function elementHasId(element, parts) {
+  if (element.tagName === parts[0]) {
+    elements.push(element);
+  }
+}
+
+function elementHasClass(elements, elementsFound, parts) {
+  for (i = 0; i < elementsFound.length; i++) {
+    if (elementsFound[i].tagName === parts[0].toUpperCase()) {
+      elements.push(elementsFound[i]);
     }
   }
 }
