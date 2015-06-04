5ce166d90fe3f68ebefd95e3dae28799346b54fa
tommasobratto <tbratto22@gmail.com>
2015-06-04 16:36:49 +0100

    added new getElementById and getElementsByTag functions

--- Answer.js
+++ Answer.js
@@ -1,166 +1,85 @@
 var $ = function (selector) {
   var elements = [];
-  var parts;
+  var parts = [];
   var result;
 
   if (selector.includes("#") && !selector.includes(".")) {
 
     parts = selector.replace("#", " #").match(/\S+/g);
-    result = cssEngine.elementByIdFinder(parts);
-    elements.push(result);
+    elements = cssEngine.getElementById(parts);
 
   } else if (selector.includes(".") && !selector.includes("#")) {
 
     parts = selector.replace(".", " .").match(/\S+/g);
-    elements = cssEngine.elementByClassFinder(parts);
 
   } else if (selector.includes("#") && selector.includes(".")) {
 
     parts = selector.replace("#", " #").replace(".", " .").match(/\S+/g);;
-    elements = cssEngine.elementMatcher(parts);
 
   } else {
-    elements = cssEngine.elementByTagFinder(selector);
+
+    parts.push(selector);
+    elements = cssEngine.getElementsByTag(parts);
   }
 
+  // console.log(elements);
   return elements;
 }
 
 cssEngine = function() {
+  var tagName, idName, className;
 
-  function elementByIdFinder(parts) {
-    var element, result, firstToken, secondToken;
+  function getElementsByTag(selector) {
+    tagName = getTagName(selector);
+    var result = document.getElementsByTagName(tagName);
 
-    firstToken = parts[0];
-    secondToken = parts[1];
+    return [].slice.call(result);
+  }
 
-    if (firstToken[0] !== "#") {
-      element = getElementById(secondToken);
-      result = elementHasTag(element, secondToken);
-    } else {
-      element = getElementById(firstToken);
-      result = element;
+  function getElementById(parts) {  
+    var result = [];
+    tagName = getTagName(parts);
+    idName = getIdName(parts);
+
+    var element = document.getElementById(idName);
+
+    if (!tagName || (tagName.toUpperCase() === element.tagName)) {
+      result.push(element);
     }
     return result;
   }
 
-  function elementByClassFinder(parts) {
-    var elementsFound, result, firstToken, secondToken;
-
-    firstToken = parts[0];
-    secondToken = parts[1];
-
-    if (firstToken[0] !== ".") {
-      elementsFound = getElementsByClass(secondToken);
-
-      result = elementsHaveTag(elementsFound, firstToken);
-    } else {
-      elementsFound = getElementsByClass(firstToken);
-
-      result = [].slice.call(elementsFound);
-    }
-    return result;
-  }
-
-  function elementMatcher(parts) {
-    var element, classElements, result, firstToken, secondToken, thirdToken;
-
-    firstToken = parts[0];
-    secondToken = parts[1];
-    thirdToken = parts[2];
-
-    if (thirdToken) {
-      if (secondToken[0] === "#") {
-        element = getElementById(secondToken);
-        classElements = getElementsByClass(thirdToken);
-
-        if (element.tagName === firstToken.toUpperCase()) {
-          result = matchElements(element, classElements);
-        }
-      } else {
-        element = getElementById(thirdToken);
-        classElements = getElementsByClass(secondToken);
-
-        if (element.tagName === firstToken.toUpperCase()) {
-          result = matchElements(element, classElements);
-        }
+  function getTagName(parts) {
+    tagName = parts.filter(function(token) {
+      if (!token.includes("#") && !token.includes(".")) {
+        return token;
       }
-    } else {
-      if (firstToken === "#") {
-        element = getElementById(secondToken);
-        classElements = getElementsByClass(thirdToken);
+    });
 
-        result = matchElements(element, classElements);
-      } else {
-        element = getElementById(thirdToken);
-        classElements = getElementsByClass(secondToken);
+    return tagName[0];
+  }
 
-        result = matchElements(element, classElements);
+  function getIdName(parts) {
+    idName = parts.filter(function(token) {
+      if (token.includes("#")) {
+       return token;
       }
-    }
-    return result;
+    });
+
+    return idName[0].replace("#", "");
   }
 
-  function elementByTagFinder(selector) {
-    var elementsFound;
-
-    elementsFound = document.getElementsByTagName(selector);
-
-    return [].slice.call(elementsFound);
-  }
-
-
-
-  function getElementById(token) {
-    var elementId;
-
-    elementId = token.replace("#", "");
-    return document.getElementById(elementId);
-  }
-
-  function getElementsByClass(token) {
-    var elementClass;
-
-    elementClass = token.replace(".", "");
-    return document.getElementsByClassName(elementClass);
-  }
-
-
-
-  function elementHasTag(element, firstToken) {
-    if (element.tagName === firstToken.toUpperCase()) {
-      return element;
-    } else {
-      return [];
-    }
-  }
-
-  function elementsHaveTag(elementsFound, firstToken) {
-    filterByTag = function(element) {
-      if (element.tagName === firstToken.toUpperCase()) {
-        return element;
-      }    
-    }
-
-    var filteredResult = [].slice.call(elementsFound).filter(filterByTag);
-    return filteredResult;
-  }
-
-  function matchElements(element, classElements) {
-    filterById = function(classElement) {
-      if (classElement.id === element.id) {
-        return classElement;
+  function getClassName(parts) {
+    className = parts.filter(function(token) {
+      if (token.includes(".")) {
+        return token;
       }
-    }
-    var filteredResult = [].slice.call(classElements).filter(filterById);
-
-    return filteredResult;
+    });
+    return className;
   }
 
   return {
-    elementByIdFinder: elementByIdFinder,
-    elementByClassFinder: elementByClassFinder,
-    elementMatcher: elementMatcher,
-    elementByTagFinder: elementByTagFinder
+    getElementsByTag: getElementsByTag,
+    getElementById: getElementById
   }
 }();
